152436140b04f88f9893c68c551e61094e8395b3
vicence <tessvicence@gmail.com>
2013-03-27 14:17:43 +0800

    first.c

---
+++ first.c
@@ -0,0 +1,39 @@
+/*
+//  (1)版权信息 
+//	(2)文件名称，标识符，摘要 
+//	(3)当前版本号，作者/修改者，完成日期 
+//	(4)版本历史信息 
+//
+//
+//	(1)版权信息 
+//	Copyright (c) 2013, vicence
+//	All rights reserved.
+//	tessvicence@gmail.com
+//
+//	(2)文件名称，标识符，摘要 
+//	文件名称：first.c 
+//	文件标识： 
+//	摘	  要：简介文件内容 
+//
+//	(3)当前版本 
+//	当前版本：1.1 
+//	作	  者：vicence 
+//	修改时间：2013.3.27
+//
+//	(4)历史版本信息 
+//	取代版本：1.0 
+//	作	  者：vicence 
+// 	修改时间：2013.3.26 
+*/
+
+#include <stdio.h> 
+int main (void)
+{
+	int num;
+	num = 1;
+	
+	printf ("I am a simple ");
+	printf ("computer.\n");
+	printf ("My faovrite number is %d baceuse it is first.\n", num);
+	return 0;
+}
