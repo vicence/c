c972a0a298cb3869b5da6b2b29e1f0df58b26e11
vicence <tessvicence@gmail.com>
2013-04-19 18:56:36 +0800

    Create while_first.c

---
+++ while_first.c/while_first.c
@@ -0,0 +1,10 @@
+//while_first.c -- 心情有点遭，写个while语句来调侃一下
+#include <stdio.h>
+int main (void)
+{
+        while (1)
+        {
+                printf ("心情有点遭，还没找到为什么，写个while语句来调侃一下\n");
+        }
+        return 0;
+}
